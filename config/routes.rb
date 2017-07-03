1a88921c26368fb2f3f66bb2462d32f49ff67571
HyunWoo <chaosphch@gmail.com>
2017-07-03 21:29:35 +0900

    additional routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,24 @@
 Rails.application.routes.draw do
-  resources :upcoming_artists
-  resources :upcoming_comments
-  resources :feed_comments
-  resources :feed_likes
-  resources :feed_artists
-  resources :curation_comments
-  resources :curation_likes
-  resources :curation_videos
   root 'feeds#index'
   resources :recommended_urls
-  resources :curations
-  resources :artists
-  resources :feeds
-  resources :feeds do
+  resources :curations do
+    resources :curation_video
+    resources :curation_like
+    resources :curation_comment
   end
-  resources :upcomings
+  resources :artists
+  resources :feeds do
+    resources :feed_artist
+    resources :feed_like
+    resources :feed_comment
+  end
   resources :upcomings do
+    resources :upcoming_comment
+    resources :upcoming_artist
   end
   devise_for :users
+  resources :users do
+    resources :recommended_url
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
