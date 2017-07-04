deed3b6a072b0b465218981dc23653c4ed5e1405
JWWon <wonjiwn@naver.com>
2017-07-04 15:08:57 +0900

    install jquery, err fixed

--- app/assets/javascripts/infinite_scroll.js
+++ app/assets/javascripts/infinite_scroll.js
@@ -1,16 +1,17 @@
 document.addEventListener("DOMContentLoaded", function(event) {
     var isLoading = false;
-    if ($('.infinite-scrolling').size() > 0) {
-        $(window).on('scroll', function() {
-            var more_contents_url = $('.pagination a.next_page').attr('href');
-            if (!isLoading && more_contents_url && $(window).scrollTop() > $(document).height() - $(window).height() - 240) {
-                isLoading = true;
-                $.getScript(more_contents_url).done(function (data,textStatus,jqxhr) {
-                    isLoading = false;
-                }).fail(function() {
-                    isLoading = false;
-                });
-            }
-        });
-    }
+    // if ($('.infinite-scrolling').size() > 0) {
+    //
+    // }
+    $(window).on('scroll', function() {
+        var more_contents_url = $('.pagination a.next_page').attr('href');
+        if (!isLoading && more_contents_url && $(window).scrollTop() > $(document).height() - $(window).height() - 240) {
+            isLoading = true;
+            $.getScript(more_contents_url).done(function (data,textStatus,jqxhr) {
+                isLoading = false;
+            }).fail(function() {
+                isLoading = false;
+            });
+        }
+    });
 });
