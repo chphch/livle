25dd9a09c45e01d52758219855892b4c32e432b1
JWWon <wonjiwn@naver.com>
2017-07-10 17:14:15 +0900

    setting complete

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -1,6 +1,10 @@
 class FeedsController < ApplicationController
   def index
-    @feeds = Feed.all
+    @feeds = Feed.paginate(page: params[:page], per_page: 15)
+    respond_to do |format|
+      format.html
+      format.js { render 'feeds/scroll_feed' }
+    end
   end
 
   def watch
