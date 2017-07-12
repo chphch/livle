620398f49c180e692f3f348b56cfed2628545d43
HyunWoo <chaosphch@gmail.com>
2017-07-12 15:05:05 +0900

    facebook login completed

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,4 +14,15 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable,
          :omniauthable, :omniauth_providers => [:facebook]
 
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+      user.nickname = auth.info.name   # assuming the user model has a name
+      user.profile_img = auth.info.image # assuming the user model has an image
+      # If you are using confirmable and the provider(s) you use validate emails,
+      # uncomment the line below to skip the confirmation emails.
+      # user.skip_confirmation!
+    end
+  end
 end
