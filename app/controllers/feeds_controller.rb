30bb43e99a34ac9b4ee3402bd5a61c5198385fef
JWWon <wonjiwn@naver.com>
2017-07-12 17:01:22 +0900

    curator view modified

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -1,7 +1,7 @@
 class FeedsController < ApplicationController
   def index
-    @curations = Curation.paginate(page: params[:page], per_page: 2)
-    @feeds = Feed.paginate(page: params[:page], per_page: 4)
+    @curations = Curation.paginate(page: params[:page], per_page: 3)
+    @feeds = Feed.paginate(page: params[:page], per_page: 6)
     respond_to do |format|
       format.html
       format.js { render 'feeds/s_feed' }
