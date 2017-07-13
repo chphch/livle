38290e940d8c947bfba6bfdbefd6008793039f0a
JWWon <wonjiwn@naver.com>
2017-07-13 16:03:41 +0900

    add calc_d_day function

--- app/controllers/upcomings_controller.rb
+++ app/controllers/upcomings_controller.rb
@@ -1,15 +1,21 @@
 class UpcomingsController < ApplicationController
+  require 'date'
+
   def index
-    @upcomings = Upcoming.paginate(page: params[:page], per_page: 8)
+    @upcomings = Upcoming.order('start_date ASC').paginate(page: params[:page], per_page: 8)
     respond_to do |format|
       format.html
       format.js { render 'upcomings/s_upcoming_mobile' }
     end
+    calc_d_day
+
     render_by_device
   end
 
   def show
     @upcoming = Upcoming.find_by(id: params[:id])
+    @d_day = d_day
+
     render_by_device
   end
 
@@ -17,7 +23,23 @@ class UpcomingsController < ApplicationController
 
   end
 
-  def get_video(artist_name)
+  private
+    def calc_d_day
+      @d_day = []
+      @upcomings.each do |upcoming|
+        start_day = upcoming.start_date.strftime('%Q').to_i
+        today = DateTime.now.strftime('%Q').to_i
+        day_to_millisec = 1000*60*60*24
 
-  end
+        @d_day[upcoming.id] = ((start_day - today)/day_to_millisec).floor
+      end
+    end
+
+    def d_day
+      start_day = @upcoming.start_date.strftime('%Q').to_i
+      today = DateTime.now.strftime('%Q').to_i
+      day_to_millisec = 1000*60*60*24
+
+      return ((start_day - today)/day_to_millisec).floor
+    end
 end
--- app/models/upcoming.rb
+++ app/models/upcoming.rb
@@ -3,7 +3,5 @@ class Upcoming < ApplicationRecord
   has_many :upcoming_comments
   has_many :upcoming_artists
 
-  def calc_d_day()
-    return 12
-  end
+
 end
