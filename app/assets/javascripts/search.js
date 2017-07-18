e66491d6d55b618e79dd9de10c28ee743f258ed7
JWWon <wonjiwn@naver.com>
2017-07-19 06:52:13 +0900

    search_card js problem.. fixed

--- app/assets/javascripts/search.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-    var width = $('.search-card-image-container').css('width');
-    $('.search-card-image-container').css({
-        'height': width
-    });
-});
