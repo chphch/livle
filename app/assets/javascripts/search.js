88547e6045f5b1f9d6784d911ea008bdcfb154be
JWWon <wonjiwn@naver.com>
2017-08-02 19:20:44 +0900

    js event handler updated

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -5,16 +5,27 @@ document.addEventListener("DOMContentLoaded", function(event) {
             'flex-grow': 0,
             '-webkit-flex-grow': 0
         });
-        $('.search-input-field').css('width', '80%');
-        $('.input-esc').show();
+        $('.search-input-field').css('width', '84%');
+        $('.cancel-field').show();
     });
     //focus 안할 때
-    $('.input-field').blur(function () {
+    $('.cancel-field').click(function () {
         $('.input-icon-container').css({
             'flex-grow': 2,
             '-webkit-flex-grow': 2
         });
         $('.search-input-field').css('width', '100%');
-        $('.input-esc').hide();
+        $('.cancel-field').hide();
+    });
+
+    $('.input-field').bind('input', function () {
+        if($(this).val().length > 0) {
+            $('.input-esc').show();
+        } else {
+            $('.input-esc').hide();
+        }
+    });
+    $('.input-esc').click(function () {
+        $('.input-field').val('');
     });
 });
