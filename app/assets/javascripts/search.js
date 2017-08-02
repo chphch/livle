394353e91442de7fff7e1f864907f34795300f91
JWWon <wonjiwn@naver.com>
2017-08-02 18:53:31 +0900

    err fixed

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,13 +1,19 @@
 document.addEventListener("DOMContentLoaded", function(event) {
     //focus 할 때
     $('.input-field').focus(function () {
-        $('.input-icon-container').css('flex-grow', 0);
+        $('.input-icon-container').css({
+            'flex-grow': 0,
+            '-webkit-flex-grow': 0
+        });
         $('.search-input-field').css('width', '80%');
         $('.input-esc').show();
     });
     //focus 안할 때
     $('.input-field').blur(function () {
-        $('.input-icon-container').css('flex-grow', 2);
+        $('.input-icon-container').css({
+            'flex-grow': 2,
+            '-webkit-flex-grow': 2
+        });
         $('.search-input-field').css('width', '100%');
         $('.input-esc').hide();
     });
