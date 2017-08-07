3a8beaef5fc046c541d79eee4e30f588739cbe5a
JWWon <wonjiwn@naver.com>
2017-08-07 17:17:51 +0900

    feed, upcoming UI updated

--- app/assets/javascripts/infinite_scroll.js
+++ app/assets/javascripts/infinite_scroll.js
@@ -4,6 +4,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
         $(window).on('scroll', function() {
             var more_contents_url = $('.pagination a.next_page').attr('href');
             if (!isLoading && more_contents_url && $(window).scrollTop() > $(document).height() - $(window).height() - 240) {
+                console.log("url: " + more_contents_url);
                 isLoading = true;
                 $.getScript(more_contents_url).done(function (data,textStatus,jqxhr) {
                     isLoading = false;
