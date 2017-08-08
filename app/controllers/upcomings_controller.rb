3d6bc45d3c5b241bd47604ea1b0ac4beadc610ff
JWWon <wonjiwn@naver.com>
2017-08-08 14:42:57 +0900

    add admin upcoming

--- app/controllers/upcomings_controller.rb
+++ app/controllers/upcomings_controller.rb
@@ -1,11 +1,10 @@
 class UpcomingsController < ApplicationController
-  require 'date'
+  helper_method :d_day
 
   def index
     today = DateTime.now
     @upcomings = Upcoming.where('start_date >= ?', today).order('start_date ASC')
                      .paginate(page: params[:page], per_page: 8)
-    calc_d_day
     respond_to do |format|
       format.html { render_by_device }
       format.js { render 'upcomings/s_upcoming_mobile' }
@@ -18,7 +17,6 @@ class UpcomingsController < ApplicationController
     @like_true = user_signed_in? ?
       UpcomingLike.where(upcoming_id: params[:id], user_id: current_user.id).take :
       false
-    @d_day = d_day
     @disable_nav = true
     render_by_device
   end
@@ -27,25 +25,16 @@ class UpcomingsController < ApplicationController
 
   end
 
-  private
-    def calc_d_day
-      @d_day = []
-      @upcomings.each do |upcoming|
-        start_day = upcoming.start_date.strftime('%Q').to_i
-        today = DateTime.now.strftime('%Q').to_i
-        day_to_millisec = 1000*60*60*24
+  def d_day(start_date)
+    start_day = start_date.strftime('%Q').to_i
+    today = DateTime.now.strftime('%Q').to_i
+    day_to_millisec = 1000*60*60*24
 
-        d_day = ((start_day - today)/day_to_millisec).floor
-        @d_day[upcoming.id] = -d_day
-      end
-    end
-
-    def d_day
-      start_day = @upcoming.start_date.strftime('%Q').to_i
-      today = DateTime.now.strftime('%Q').to_i
-      day_to_millisec = 1000*60*60*24
-
-      d_day = ((start_day - today)/day_to_millisec).floor
+    d_day = ((start_day - today)/day_to_millisec).floor
+    if d_day == 0
+      return "-day"
+    else
       return -d_day
     end
+  end
 end
