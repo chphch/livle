db2d97662af66ebbef70b27d4fb4ad34d41fb4bc
JWWon <wonjiwn@naver.com>
2017-08-16 02:14:09 +0900

    desktop navbar settings done

--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -1,6 +1,4 @@
 document.addEventListener("DOMContentLoaded", function(event) {
     //navbar_desktop.html.erb
-    $('._navbar-search-container').click(function () {
-        $('._navbar-search-input').focus();
-    });
+
 });
