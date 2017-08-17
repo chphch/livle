b967eaea4a55de47b36c477df0fc65208f32eee9
JWWon <wonjiwn@naver.com>
2017-08-17 16:02:25 +0900

    modify curation tab & feed padding updated

--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -1,4 +1,10 @@
 document.addEventListener("DOMContentLoaded", function(event) {
     //navbar_desktop.html.erb
-
+    $(window).on('scroll', function() {
+        if( $(window).scrollTop === $(window).height() * 0.05 ) {
+            $('._navbar').css('background', 'rgba(0, 0, 0, 1)');
+        } else {
+            $('._navbar').css('background', 'rgba(0, 0, 0, 0)');
+        }
+    });
 });
