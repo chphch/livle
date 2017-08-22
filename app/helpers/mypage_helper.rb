a0a130e58b26f6cae14ded8e957fbf45fa8ce1bc
JWWon <wonjiwn@naver.com>
2017-08-23 04:59:58 +0900

    contact email -> copy to clipboard done

--- app/helpers/mypage_helper.rb
+++ app/helpers/mypage_helper.rb
@@ -1,2 +1,12 @@
 module MypageHelper
+  require 'clipboard'
+
+  def copyEmail(email)
+    Clipboard.copy(email)
+    puts "Email copied: " + email
+  end
+
+  def pasteEmail
+    return Clipboard.paste
+  end
 end
