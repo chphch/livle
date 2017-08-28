d58c668436a7a472a1c4288d6a0796a5faca7967
JWWon <wonjiwn@naver.com>
2017-08-29 02:25:12 +0900

    덧글 달 때 ajax 처리, 덧글내용 및 모델 생성이 제대로 안됨

--- app/controllers/feed_comments_controller.rb
+++ app/controllers/feed_comments_controller.rb
@@ -1,3 +1,12 @@
 class FeedCommentsController < ApplicationController
+  def create
+    @content = params[:content]
+    @comment = FeedComment.new(content: @content, feed_id: params[:model_id],
+                               user_id: params[:user_id])
+    @comment.save
 
+    respond_to do |format|
+      format.js { render 'xhrs/append_comment_mobile' }
+    end
+  end
 end
--- app/controllers/upcoming_comments_controller.rb
+++ app/controllers/upcoming_comments_controller.rb
@@ -1,2 +1,12 @@
 class UpcomingCommentsController < ApplicationController
+  def create
+    @content = params[:content]
+    @comment = UpcomingComment.new(content: @content, upcoming_id: params[:model_id],
+                                   user_id: params[:user_id])
+    @comment.save
+
+    respond_to do |format|
+      format.js { render 'xhrs/append_comment_mobile' }
+    end
+  end
 end
