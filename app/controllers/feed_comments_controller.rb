7cce49ecdc9625fa39dc931608c7ed7a37cae597
JWWon <wonjiwn@naver.com>
2017-08-30 16:19:29 +0900

    modify comment add logic & style

--- app/controllers/feed_comments_controller.rb
+++ app/controllers/feed_comments_controller.rb
@@ -1,14 +1,15 @@
 class FeedCommentsController < ApplicationController
   def create
     params.require(:feed_comment).permit(:content, :model_id, :user_id)
-    @comment = FeedComment.new(content: params[:feed_comment][:content], feed_id: params[:feed_comment][:model_id],
-                               user_id: params[:feed_comment][:user_id])
-    @comment.save
-
-    @content = @comment.content
-    @user = User.find(@comment.user_id)
-    respond_to do |format|
-      format.js { render 'xhrs/append_comment_mobile' }
+    if params[:feed_comment][:user_id] != -1
+      @comment = FeedComment.new(content: params[:feed_comment][:content], feed_id: params[:feed_comment][:model_id],
+                                 user_id: params[:feed_comment][:user_id])
+      @comment.save
+      @content = @comment.content
+      @user = User.find(@comment.user_id)
+      respond_to do |format|
+        format.js { render 'xhrs/append_comment_mobile' }
+      end
     end
   end
 end
--- app/controllers/upcoming_comments_controller.rb
+++ app/controllers/upcoming_comments_controller.rb
@@ -1,14 +1,15 @@
 class UpcomingCommentsController < ApplicationController
   def create
     params.require(:upcoming_comment).permit(:content, :model_id, :user_id)
-    @comment = UpcomingComment.new(content: params[:upcoming_comment][:content], upcoming_id: params[:upcoming_comment][:model_id],
-                                   user_id: params[:upcoming_comment][:user_id])
-    @comment.save
-
-    @content = @comment.content
-    @user = User.find(@comment.user_id)
-    respond_to do |format|
-      format.js { render 'xhrs/append_comment_mobile' }
+    if params[:upcoming_comment][:user_id] != -1
+      @comment = UpcomingComment.new(content: params[:upcoming_comment][:content], upcoming_id: params[:upcoming_comment][:model_id],
+                                     user_id: params[:upcoming_comment][:user_id])
+      @comment.save
+      @content = @comment.content
+      @user = User.find(@comment.user_id)
+      respond_to do |format|
+        format.js { render 'xhrs/append_comment_mobile' }
+      end
     end
   end
 end
