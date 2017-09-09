49addd1653ed0de6f4beb4aec398133986cc2f76
JWWon <wonjiwn@naver.com>
2017-09-09 16:33:54 +0900

    prepend comment works fine

--- app/assets/javascripts/textarea.js
+++ app/assets/javascripts/textarea.js
@@ -1,7 +1,6 @@
 document.addEventListener("DOMContentLoaded", function(event) {
     //textarea resize
     $("textarea.autosize").on('keydown keyup', function () {
-        console.log('resize textarea!');
         $(this).height(1).height( $(this).prop('scrollHeight') );
     });
 });
