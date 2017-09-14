30c36f7f72f47ad679067c385b5ca2fa9864c872
Jaeseong Seo <07js23@gmail.com>
2017-09-14 23:09:29 +0900

    Add alternative_name on Artist

--- db/migrate/20170703071804_create_artists.rb
+++ db/migrate/20170703071804_create_artists.rb
@@ -4,6 +4,7 @@ class CreateArtists < ActiveRecord::Migration[5.1]
       t.belongs_to :curation
       t.belongs_to :artist
       t.string :name
+      t.string :alternative_name
       t.string :image_url
 
       t.timestamps
