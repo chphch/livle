26da4a626f8da872cfd322210fb20321edce0a33
JWWon <wonjiwn@naver.com>
2017-09-22 02:15:37 +0900

    error page update

--- config/routes.rb
+++ config/routes.rb
@@ -61,4 +61,7 @@ Rails.application.routes.draw do
   resources :notices, only: [:index, :create, :destroy]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get 'errors/not_found'
+  match '/404', to: 'errors#not_found', via: :all
 end
