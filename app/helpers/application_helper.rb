ae1c11ece2f56ad72e0e4cfceaf9617946759545
Jaeseong Seo <07js23@gmail.com>
2017-09-21 20:55:30 +0900

    Thumbnail two sizes, and opengraph for sharing implemented

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,6 +15,40 @@ module ApplicationHelper
     @devise_mapping ||= Devise.mappings[:user]
   end
 
+  def og_title(title = "Livle : Life is live")
+    "<meta property='og:title' content='#{title}'>"
+  end
+
+  def og_description(desc = '라이블은 라이브 음악을 쉽고 재밌게 즐기는 방법을 제공하는 라이브 음악 플랫폼입니다.
+    질좋은 라이브 음악 영상을 감상하고 콘서트 티켓을 쉽게 구매할 수 있습니다.')
+    "<meta property='og:description' content='#{desc}'>"
+  end
+
+  def og_image(url)
+    unless url
+      url = "#{request.base_url}/logo_og.jpg"
+    end
+    "<meta property='og:image' content='#{url}'>"
+  end
+
+  def opengraph(content)
+    if content.class == Feed
+      content_for :opengraph, "#{og_title(content.title)}
+      #{og_description(content.content)}
+      #{og_image(content.youtube_url)}".html_safe
+    elsif content.class == Upcoming
+      content_for :opengraph, "#{og_title(content.title)}
+      #{og_description}
+      #{og_image(content.main_video ? content.main_video.youtube_url : content.image_url)}".html_safe
+    end
+  end
+
+  def yield_opengraph
+    content_for?(:opengraph) ? content_for(:opengraph) : "#{og_title}
+    #{og_description}
+    #{og_image(nil)}".html_safe
+  end
+
   def thumbnail_tag(youtube_url, options = {})
     video_id = get_youtube_video_id(youtube_url)
     # TODO : youtube-thumbnail 클래스에 인라인 스타일 옮기기
@@ -27,7 +61,7 @@ module ApplicationHelper
     width: 100%;
     height: 0;
     padding-bottom: 67%;
-    background:url(\"#{asset_url('thumbnail_livle')}\") no-repeat center center;
+    background:url(\"#{options[:wide] ? asset_url('thumbnail_livle_wide') : asset_url('thumbnail_livle_basic')}\") no-repeat center center;
     background-size: cover'></div>".html_safe
   end
 
