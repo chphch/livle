fc1e8bacc52ed864532a3183512004552e98458b
Jaeseong Seo <07js23@gmail.com>
2017-09-21 19:11:03 +0900

    Indentation

--- app/assets/javascripts/InfiniteScroll.js
+++ app/assets/javascripts/InfiniteScroll.js
@@ -1,17 +1,17 @@
 document.addEventListener("turbolinks:load", function(event) {
-    var isLoading = false;
-        if ($('#infinite-scrolling').length > 0) {
-        $(window).on('scroll', function() {
-            var more_contents_url = $('.pagination a.next_page').attr('href');
-            if (!isLoading && more_contents_url && $(window).scrollTop() > $(document).height() - $(window).height() - 240) {
-                sendScrollEvent($(location), more_contents_url);
-                isLoading = true;
-                $.getScript(more_contents_url).done(function (data,textStatus,jqxhr) {
-                    isLoading = false;
-                }).fail(function() {
-                    isLoading = false;
-                });
-            }
+  var isLoading = false;
+  if ($('#infinite-scrolling').length > 0) {
+    $(window).on('scroll', function() {
+      var more_contents_url = $('.pagination a.next_page').attr('href');
+      if (!isLoading && more_contents_url && $(window).scrollTop() > $(document).height() - $(window).height() - 240) {
+        sendScrollEvent($(location), more_contents_url);
+        isLoading = true;
+        $.getScript(more_contents_url).done(function (data,textStatus,jqxhr) {
+          isLoading = false;
+        }).fail(function() {
+          isLoading = false;
         });
-    }
+      }
+    });
+  }
 });
