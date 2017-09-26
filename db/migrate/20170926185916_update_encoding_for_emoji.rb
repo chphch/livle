437d2888cbe225e463756a706f35c99424427897
JWWon <wonjiwn@naver.com>
2017-09-27 04:28:50 +0900

    update migrate file

--- db/migrate/20170926185916_update_encoding_for_emoji.rb
+++ db/migrate/20170926185916_update_encoding_for_emoji.rb
@@ -7,11 +7,11 @@ class UpdateEncodingForEmoji < ActiveRecord::Migration[5.1]
       execute "ALTER TABLE #{table} CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_bin"
     end
 
-    # execute "ALTER TABLE connect_urls CHANGE describe VARCHAR(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin"
-    # execute "ALTER TABLE feed_comments CHANGE content VARCHAR(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin"
-    # execute "ALTER TABLE notices CHANGE content VARCHAR(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin"
-    # execute "ALTER TABLE temporary_upcomings CHANGE artist_info VARCHAR(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin"
-    # execute "ALTER TABLE upcoming_comments CHANGE content VARCHAR(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin"
-    # execute "ALTER TABLE users CHANGE introduce VARCHAR(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin"
+    execute "ALTER TABLE 'connect_urls' CHANGE 'describe' VARCHAR(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin"
+    execute "ALTER TABLE 'feed_comments' CHANGE 'content' VARCHAR(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin"
+    execute "ALTER TABLE 'notices' CHANGE 'content' VARCHAR(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin"
+    execute "ALTER TABLE 'temporary_upcomings' CHANGE 'artist_info' VARCHAR(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin"
+    execute "ALTER TABLE 'upcoming_comments' CHANGE 'content' VARCHAR(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin"
+    execute "ALTER TABLE 'users' CHANGE 'introduce' VARCHAR(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin"
   end
 end
