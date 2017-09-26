ba8d5fc604085642f6dfe3a4c94b3ace1355d932
JWWon <wonjiwn@naver.com>
2017-09-27 04:11:05 +0900

    migration file update

---
+++ db/migrate/20170926185916_update_encoding_for_emoji.rb
@@ -0,0 +1,17 @@
+class UpdateEncodingForEmoji < ActiveRecord::Migration[5.1]
+  def change
+    # for each table
+    tables = ['feeds', 'feed_comments', 'notices', 'recent_keywords', 'temporary_upcomings',
+              'upcoming_comments', 'upcomings', 'users']
+    tables.each do |table|
+      execute "ALTER TABLE #{table} CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_bin"
+    end
+
+    # execute "ALTER TABLE connect_urls CHANGE describe VARCHAR(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin"
+    # execute "ALTER TABLE feed_comments CHANGE content VARCHAR(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin"
+    # execute "ALTER TABLE notices CHANGE content VARCHAR(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin"
+    # execute "ALTER TABLE temporary_upcomings CHANGE artist_info VARCHAR(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin"
+    # execute "ALTER TABLE upcoming_comments CHANGE content VARCHAR(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin"
+    # execute "ALTER TABLE users CHANGE introduce VARCHAR(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin"
+  end
+end
