8fc268298f9a456d0b891b851e66dbce3f0b3a6e
JWWon <wonjiwn@naver.com>
2017-09-30 03:21:34 +0900

    깜빡하고 마스터에 해서 죄송합니다.. youtube mobile showTimer update

--- app/assets/javascripts/youtube_player.js
+++ app/assets/javascripts/youtube_player.js
@@ -143,16 +143,16 @@ function onAllPlayerReady() {
             });
             player.progressLeft.on('touchend', function (e) {
                 var now = Date.now(); //현재시각
+                e.stopPropagation();
                 if (now - lastTouchEnd <= delayTime) {
-                    e.preventDefault();
                     onDoubleTabProgress(player, 'left');
                 }
                 lastTouchEnd = now; //update lastTouchEnd
             });
             player.progressRight.on('touchend', function (e) {
                 var now = Date.now(); //현재시각
+                e.stopPropagation();
                 if (now - lastTouchEnd <= delayTime) {
-                    e.preventDefault();
                     onDoubleTabProgress(player, 'right');
                 }
                 lastTouchEnd = now; //update lastTouchEnd
@@ -194,27 +194,55 @@ function onClickPlayButton(player) {
 }
 
 // on dblclick progress time
+var lt, rt, isAnimation = false;
 function onDoubleTabProgress(player, dir) {
     var curTime = player.getCurrentTime();
-    function animateLeftArrow(i) {
-        $('.arrow-left-'+i).stop().animate({'opacity': 0}, 100*(i-1)).
-        animate({'opacity': 1}, 400).animate({'opacity': 0}, 400);
+    var arrow_num = 2;
+
+    player.seekTo(dir === 'left' ? curTime - 10 : curTime + 10);
+    getSkipTime(dir);
+    for(var i = 1; i <= arrow_num; i++) {
+        animateArrow(i, dir);
     }
-    function animateRightArrow(i) {
-        $('.arrow-right-'+i).stop().animate({'opacity': 0}, 100*(i-1)).
-        animate({'opacity': 1}, 400).animate({'opacity': 0}, 400);
-    }
+}
+function animateArrow(i, dir) {
+    if (dir === 'left') $arrow = $('.arrow-left-'+i);
+    else $arrow = $('.arrow-right-'+i);
+
+    $arrow.stop(true, true).animate({'opacity': 0}, 100*(i-1)).
+    animate({'opacity': 1}, 400).animate({'opacity': 0}, 400);
+
+}
+function getSkipTime(dir) {
+    var $time, $timer;
     if (dir === 'left') {
-        player.seekTo(curTime - 10);
-        for(var i = 1; i < 3; i++) {
-            animateLeftArrow(i);
-        }
+        $time = $('.left-skip-time');
+        $timer = $('.left-skip-timer');
+        window.clearTimeout(lt);
     } else {
-        player.seekTo(curTime + 10);
-        for(var i = 1; i < 3; i++) {
-            animateRightArrow(i);
-        }
+        $time = $('.right-skip-time');
+        $timer = $('.right-skip-timer');
+        window.clearTimeout(rt);
     }
+
+    if (isAnimation) {
+        current_skip_time = $time.html()*1 + 10;
+        $time.text(current_skip_time);
+    } else {
+        $time.text(10);
+    }
+
+    isAnimation = true;
+    $timer.stop(true, true).animate({'opacity': 1}, 50)
+        .delay(750).animate({'opacity': 0}, 200);
+    if (dir === 'left') lt = window.setTimeout(function () {
+        isAnimation = false;
+        $time.text(10);
+    }, 1500);
+    else rt = window.setTimeout(function () {
+        isAnimation = false;
+        $time.text(10);
+    }, 1500);
 }
 
 // on click quality button
