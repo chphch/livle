809fb9347aff28c315ba15ebb5dcb7140d8d7c5a
JWWon <wonjiwn@naver.com>
2017-10-03 22:11:02 +0900

    admin upcoming image_url upload update

--- app/controllers/upcomings_controller.rb
+++ app/controllers/upcomings_controller.rb
@@ -40,8 +40,8 @@ class UpcomingsController < ApplicationController
 
   def update
     @upcoming = Upcoming.find(params[:id])
-    if @upcoming.update(title: params[:upcoming][:title], place: params[:upcoming][:place],
-                        main_youtube_id: params[:upcoming][:main_youtube_id],
+    if @upcoming.update(title: params[:upcoming][:title], image_url: params[:upcoming][:image_url],
+                        place: params[:upcoming][:place], main_youtube_id: params[:upcoming][:main_youtube_id],
                       start_date: params[:upcoming][:start_date], end_date: params[:upcoming][:end_date])
       redirect_back(fallback_location: root_path)
     else
