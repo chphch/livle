1a81be304786d55756523a56648fb8faed2e54a1
JWWon <wonjiwn@naver.com>
2017-10-05 01:24:26 +0900

    mobile ticket#info update

--- app/controllers/ticket_controller.rb
+++ app/controllers/ticket_controller.rb
@@ -1,11 +1,13 @@
 class TicketController < ApplicationController
   def info
     @upcoming = Upcoming.find(params[:id])
+    @disable_background_image = true #for mobile
     render_by_device
   end
 
   def book
     @upcoming = Upcoming.find(params[:id])
+    @disable_background_image = true #for mobile
     render_by_device
   end
 end
