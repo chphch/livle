c8c706c0d22ec129758822193b0abbb49750c69f
JWWon <wonjiwn@naver.com>
2017-10-06 16:40:32 +0900

    delete custom class

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,12 +1,9 @@
-require 'feed_status'
 class AdminController < ApplicationController
   before_action :is_admin
-  helper_method :get_feed_status, :set_feed_status
 
   def feed
     @official_feeds =  Feed.where(is_curation: true).order('rank DESC').paginate(page: params[:page], per_page: 30)
     @common_feeds = Feed.where(is_curation: false).order('rank DESC').paginate(page: params[:page], per_page: 30)
-    @feed_status = FeedStatus.new #for remember status
     render_by_device
   end
 
@@ -42,14 +39,5 @@ class AdminController < ApplicationController
   def notice
     @notices = Notice.all
     render_by_device
-  end
-
-  # helper
-  def get_feed_status
-    @feed_status.get_status
-  end
-
-  def set_feed_status(type)
-    @feed_status.set_status(type)
   end
 end
