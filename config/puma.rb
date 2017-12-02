224ab5f5b57d1b579ad0755cc64f23f8c3c05cb2
Jaeseong Seo <07js23@gmail.com>
2017-12-02 15:18:56 +0900

    puma setting

--- config/puma.rb
+++ config/puma.rb
@@ -56,5 +56,5 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 plugin :tmp_restart
 
 # settings for EB
-bind "unix:///var/run/puma/my_app.sock"
-pidfile "/var/run/puma/my_app.sock"
+#bind "unix:///var/run/puma/my_app.sock"
+#pidfile "/var/run/puma/my_app.sock"
